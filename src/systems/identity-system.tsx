d0e63e9441ba51402d822ac92e96dc50762346f9
Pavol Hejny <me@pavolhejny.com>
2022-01-27 18:09:33 +0100

    Module working

--- src/systems/identity-system.tsx
+++ src/systems/identity-system.tsx
@@ -1,5 +1,6 @@
 import { declareModule, makeModalModule, React, styled } from '@collboard/modules-sdk';
-import helloWorldIcon from '../../assets/hello-world-icon.png';
+import { spaceTrim } from 'spacetrim';
+import { v4 } from 'uuid';
 import { contributors, description, license, repository, version } from '../../package.json';
 
 declareModule(
@@ -11,27 +12,87 @@ declareModule(
             contributors,
             license,
             repository,
-            title: { en: 'Sample button' },
-            categories: ['Productivity', 'Buttons', 'Template'],
-            icon: helloWorldIcon,
+            title: 'IdentitySystem',
             flags: {
-                isTemplate: true,
+                isHidden: true,
             },
         },
         async createModal(systems) {
-            const { identitySystem } = await systems.request('identitySystem');
+            const { identitySystem, routingSystem } = await systems.request('identitySystem', 'routingSystem');
 
-            const { browserId, sessionId } = identitySystem;
+            const instanceIdentity = identitySystem.createInstanceIdentity();
+
             return (
                 <>
-                    <Value>
-                        <b>browserId: </b>
-                        {browserId}
-                    </Value>
-                    <Value>
-                        <b>sessionId: </b>
-                        {sessionId}
-                    </Value>
+                    {Object.entries(instanceIdentity).map(([key, value]) => (
+                        <Value {...{ key }}>
+                            <b className="col key">{key}: </b>
+                            <span className="col value">{value || 'none'}</span>
+                            <span className="col actions">
+                                {key === 'browserId' && (
+                                    <>
+                                        <button
+                                            onClick={async () => {
+                                                if (
+                                                    !confirm(
+                                                        spaceTrim(`
+                                                          Do you really want to change your browserId.
+                                                          You will loose all your boards connected with this browser!
+                                                        `),
+                                                    )
+                                                ) {
+                                                    return;
+                                                }
+
+                                                const browserId = prompt(
+                                                    spaceTrim(`
+
+                                                      Please enter a new browserId.
+
+                                                      Current browserId: ${value}
+                                                      Note: There is prefilled random sample:
+
+                                                    `),
+                                                    v4(),
+                                                );
+
+                                                if (
+                                                    !browserId ||
+                                                    !/^\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b$/.test(
+                                                        browserId,
+                                                    )
+                                                ) {
+                                                    alert(`BrowserId must be a valid UUID.`);
+                                                    return;
+                                                }
+
+                                                localStorage.removeItem(`clientId`);
+                                                localStorage.removeItem(`Collboard_ApiClientCache_getMyBoards`);
+                                                localStorage.removeItem(`Collboard_IdentitySystem_clientId`);
+                                                localStorage.removeItem(`Collboard_IdentitySystem_clientUuid`);
+
+                                                await (identitySystem as any).storage.setItem(`clientId`, browserId);
+
+                                                alert(
+                                                    spaceTrim(`
+                                                      BrowserId successfully changed!
+                                                      Now it's ${browserId}
+
+                                                      Redirecting to homepage...
+
+                                                    `),
+                                                );
+
+                                                window.location.href = routingSystem.homeUrl.href;
+                                            }}
+                                        >
+                                            Change
+                                        </button>
+                                    </>
+                                )}
+                            </span>
+                        </Value>
+                    ))}
                 </>
             );
         },
@@ -39,5 +100,14 @@ declareModule(
 );
 
 const Value = styled.div`
-    background-color: #906090;
+    border-bottom: 1px solid #eee;
+    padding: 10px;
+
+    .col {
+        display: inline-block;
+    }
+
+    .key {
+        width: 100px;
+    }
 `;
