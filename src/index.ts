5754b1a4b7c5c95c89cdd9dfb8e8e613c067aa98
Pavol Hejny <me@pavolhejny.com>
2022-09-03 00:19:33 +0200

    Finishing to deploy

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import './message-api/message-api';
-//import './systems/control-system';
-//import './systems/control-system-pressed-keys';
-//import './systems/identity-system';
+import './systems/control-system';
+import './systems/control-system-pressed-keys';
+import './systems/identity-system';
 
 /**
  * TODO: Some inteligent way how to develop multiple modal modules
